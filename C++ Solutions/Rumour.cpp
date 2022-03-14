4b79c81b076d0f3c0cc4eb054e0c7b7bfccd63de
Daniel Israelov <72816570+Daniel-israelov@users.noreply.github.com>
2022-03-14 12:01:32 +0200

    added comments

--- C++ Solutions/Rumour.cpp
+++ C++ Solutions/Rumour.cpp
@@ -7,7 +7,8 @@ typedef long long llong;
 llong rumour(llong a, llong b) {
 	if (a == b)
 		return 0;
-
+	
+	//vectors of vertices from a,b to root
 	vector<llong> aRoots;
 	vector<llong> bRoots;
 	
@@ -19,10 +20,19 @@ llong rumour(llong a, llong b) {
 		bRoots.push_back(b);
 		b /= 2;
 	}
+	//the last index of the vectors of a and b
 	int aIndex = aRoots.size() - 1;
 	int bIndex = bRoots.size() - 1;
 
 	while (aIndex >= 0 && bIndex >= 0 && aRoots.at(aIndex) == bRoots.at(bIndex)) {
+		//calculating the distance from a,b to the lowest common ancestor
+		//for example:
+		//aRoots --> [10, 5, 2, 1]
+		//bRoots -->	 [6, 3, 1]
+		//then the lowest common ancestor is 1
+		//a distance is 3
+		//b distance is 2
+		//Min dist. from a to be is 2+3=5
 		aIndex--;
 		bIndex--;
 	}
